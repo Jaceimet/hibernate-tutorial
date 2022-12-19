a26a05c58d6c286895ebb7aa6c1f65ea90d70a86
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2022-12-18 16:10:07 -0800

    Query students email with luv2code.com and gmail.com

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/QueryStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/QueryStudentDemo.java
@@ -40,6 +40,22 @@ public class QueryStudentDemo {
 			System.out.println("\n\nStudents who have last name of Doe");
 			displayStudents(theStudents);
 			
+			// query students: lastName='Doe' OR firstName='Daffy'
+			theStudents = session.createQuery("from Student s where"
+							+ " s.lastName='Doe' OR s.firstName='Daffy'").list();
+			
+			
+			System.out.println("\n\nStudents who have last name of Doe OR first name of Daffy");
+			displayStudents(theStudents);
+			
+			// query students where email like '%gmail.com'
+			theStudents = session.createQuery("from Student s where"
+					+ " s.email LIKE '%gmail.com'").list();
+			
+			System.out.println("\n\nStudents whose email ends with gmail.com");
+			displayStudents(theStudents);
+			
+			
 			//commit transaction
 			session.getTransaction().commit();
 			
